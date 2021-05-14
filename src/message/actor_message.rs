2a1acde5ca955d0841906dac0ac3476cc33e233d
Bobonium <bobonium@sers.dev>
2021-05-14 16:02:16 +0200

    Update Message Documentation

--- src/message/actor_message.rs
+++ src/message/actor_message.rs
@@ -1,2 +1,13 @@
 /// Core trait to define Messages
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust
+/// use tyractorsaur::prelude::ActorMessage;
+///
+/// struct FooBar {}
+/// impl ActorMessage for FooBar {}
+/// ```
 pub trait ActorMessage: Send + Sync {}
